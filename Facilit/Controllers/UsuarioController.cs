337b611f714dda8123381ab6c9e67ff610d41984
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2023-10-13 16:31:32 -0300

    if dos campos vazios feito

--- Facilit/Controllers/UsuarioController.cs
+++ Facilit/Controllers/UsuarioController.cs
@@ -27,65 +27,28 @@ namespace Facilit.Controllers
             return View();
 
         }
-        public ActionResult Administrador() 
+
+
+        public ActionResult VerificarLogin(Usuario class_usuario)
         {
-            string sql = "select * from tb_usuarios";
-            using(var conexao = new Conexao())
-            {
-                using (MySqlCommand cmd = new MySqlCommand(sql, conexao._conn)) 
-                {
-                    var lista_usuarios = new List<Usuario>(); //  criação de uma lista de dados de todos os usuários
-                    MySqlDataReader leitura = cmd.ExecuteReader();
-                  
-                    while(leitura.Read()) // enquanto tiver dados
-                    {
-                        var prop_usuario = new Usuario
-                        {
-                            Nome_completo = Convert.ToString(leitura["nome_completo"]),
-                            Email = Convert.ToString(leitura["email"]),
-                            Nome_Usuario = Convert.ToString(leitura["nome_usuario"]),
-                            Senha_Usuario = Convert.ToString(leitura["senha_usuario"]),
-                            Adm = Convert.ToBoolean(leitura["adm"])
-
-                        };
-                        lista_usuarios.Add(prop_usuario);
-                    }
-
-
-                }
-            }
-            return View();
-        }
-        
-        public ActionResult VerificarLogin(Usuario class_usuario) 
-        {
-            using(var conexao = new Conexao())
+            using (var conexao = new Conexao())
             {
                 string str_select = "select * from tb_usuarios where nome_usuario = @nome_usuario " +
-                    "and senha_usuario = @senha "
-                    ;
+                    "and senha_usuario = @senha";
 
                 using (MySqlCommand comando = new MySqlCommand(str_select, conexao._conn))
                 {
                     comando.Parameters.AddWithValue("@nome_usuario", class_usuario.Nome_Usuario);
 
-                    comando.Parameters.AddWithValue("@senha" ,class_usuario.Senha_Usuario);
+                    comando.Parameters.AddWithValue("@senha", class_usuario.Senha_Usuario);
 
 
                     MySqlDataReader leitura = comando.ExecuteReader();
                     leitura.Read();
                     if (leitura.HasRows)
                     {
-                        bool adm = Convert.ToBoolean(leitura["adm"]);
+                        return RedirectToAction("Registro", "Usuario");
 
-                        if (adm)
-                        {
-                            return RedirectToAction("Administrador", "Usuario");
-                        }
-                        else
-                        {
-                            return RedirectToAction("Registro", "Usuario");
-                        }
                     }
                     else
                     {
@@ -97,37 +60,41 @@ namespace Facilit.Controllers
         }
         public ActionResult RecuperarSenha()
         {
-            return View();  
+            return View();
         }
         public ActionResult NovoUsuario(Usuario usuario)
         {
-            if (usuario.Nome_completo ==string.Empty|| usuario.Email==string.Empty||usuario.Nome_Usuario ==string.Empty || usuario.Senha_Usuario == string.Empty)
+            if (string.IsNullOrWhiteSpace(usuario.Nome_Usuario) || string.IsNullOrWhiteSpace(usuario.Email) ||
+                string.IsNullOrWhiteSpace(usuario.Nome_Usuario))
             {
-                ModelState.AddModelError("campo vazio", "Existem campos vazios");
-                return View("Cadastro","Usuario");
+                return RedirectToAction("Cadastro", "Usuario");
+
             }
-
-            string sql_insert = "insert into tb_usuarios (nome_completo, email, nome_usuario, senha_usuario, adm) values " +
-                "(@nc, @em, @nu, @su, @ad)";
-            using (var conexao = new Conexao())
+         
+            else
             {
-
-                using (MySqlCommand comando = new MySqlCommand(sql_insert, conexao._conn))
+                string sql_insert = "insert into tb_usuarios (nome_completo, email, nome_usuario, senha_usuario) values " +
+                    "(@nc, @em, @nu, @su )";
+                using (var conexao = new Conexao())
                 {
-                    comando.Parameters.AddWithValue("@nc",usuario.Nome_completo);
-                    comando.Parameters.AddWithValue("@em",usuario.Email);
-                    comando.Parameters.AddWithValue("@nu",usuario.Nome_Usuario);
-                    comando.Parameters.AddWithValue("@su",usuario.Senha_Usuario);
-                    comando.Parameters.AddWithValue("@ad",usuario.Adm);
 
-                    comando.ExecuteNonQuery();
+                    using (MySqlCommand comando = new MySqlCommand(sql_insert, conexao._conn))
+                    {
+                        comando.Parameters.AddWithValue("@nc", usuario.Nome_completo);
+                        comando.Parameters.AddWithValue("@em", usuario.Email);
+                        comando.Parameters.AddWithValue("@nu", usuario.Nome_Usuario);
+                        comando.Parameters.AddWithValue("@su", usuario.Senha_Usuario);
 
-                    return RedirectToAction("Administrador", "Usuario");
+
+                        comando.ExecuteNonQuery();
+
+                        return RedirectToAction("Index", "Usuario");
+                    }
+
                 }
-                
             }
 
-        }
+        } 
         bool existe = false;
         private bool ExisteUsuario(Usuario usuario)
         {
