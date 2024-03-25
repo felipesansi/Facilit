b134c12e7611ee1c1b38fe76b76c574849b350ca
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-03-25 16:02:52 -0300

    Concluido nome da foto

--- Facilit/Controllers/WebcanController.cs
+++ Facilit/Controllers/WebcanController.cs
@@ -18,43 +18,41 @@ namespace Facilit.Controllers
         string tokenTiny = "02011b49e5399d62d999007a8952642c85cca50bc310b49fdd6c3674fdff4b2a";
 
 
-        public async Task <ActionResult> Registro()
+        public async Task<ActionResult> Registro()
         {
-           RetornoTinyApi produto = new RetornoTinyApi();
+            RetornoTinyApi produto = new RetornoTinyApi();
             var produtos = await produto.ListarProdutos(tokenTiny);
             var dropdown_produto = produtos.retorno.produtos.Select(s => new { Id = s.id, Produto = s.descricao + " | " + s.tipoVariacao }).ToList();
-            ViewBag.Produtos = new SelectList( dropdown_produto, "Id", "Produto");
+            ViewBag.Produtos = new SelectList(dropdown_produto, "Id", "Produto");
+          
             //clientes
 
             RetornoTinyApi cliente = new RetornoTinyApi();
             var clientes = await cliente.ListarClientes(tokenTiny);
-            var dropdown_cliente = clientes.retorno.contatos.Select(s => new {Id = s.contato.id, cliente =s.contato.nome }).ToList();
-            ViewBag.Clientes = new SelectList(dropdown_cliente, "Id", "Cliente");
+            var dropdown_cliente = clientes.retorno.contatos.Select(s => new { Id = s.contato.id, cliente = s.contato.nome }).ToList();
+            ViewBag.Clientes = new SelectList(dropdown_cliente, "Id", "Cliente" );
             return View();
         }
         [HttpPost]
-        public ActionResult SalvarFoto(string dados_imagem)
+        public ActionResult SalvarFoto(string dados_imagem, string produto_selecionado, string cliente_selecionado)
         {
             byte[] vet_bytes = Convert.FromBase64String(dados_imagem);
             string caminho_diretorio = Server.MapPath("~/Fotos");
-            string caminho_imagem = Path.Combine(caminho_diretorio, "Produto_foto.jpg");
-
 
             if (!Directory.Exists(caminho_diretorio))
-
             {
                 Directory.CreateDirectory(caminho_diretorio);
             }
-
-
-
-
+            DateTime data = DateTime.Now;
+         
+            string nome_arquivo = $"Produto id {produto_selecionado}_Cliente id {cliente_selecionado}.jpg";
+            string caminho_imagem = Path.Combine(caminho_diretorio, nome_arquivo);
             System.IO.File.WriteAllBytes(caminho_imagem, vet_bytes);
-            return Json(new { sucesso = true, mensagem = "Foto salva com sucesso" });
 
+            return Json(new { sucesso = true, mensagem = "Foto salva com sucesso! \n"+nome_arquivo });
         }
 
-
+    
 
     }
    
--- Facilit/Scripts/webcan.js
+++ Facilit/Scripts/webcan.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const video = document.querySelector('video');
     const canvas = document.querySelector('canvas');
     const botao = document.getElementById('btn_salvar');
-    const proporcao = 0.75; // Defina a proporção desejada
+    const proporcao = 0.75; 
 
     navigator.mediaDevices.getUserMedia({ video: true })
         .then(stream => {
@@ -17,18 +17,19 @@ document.addEventListener("DOMContentLoaded", function () {
         const videoWidth = video.videoWidth;
         const videoHeight = video.videoHeight;
         const capturaWidth = videoWidth;
-        const capturaHeight = videoWidth * proporcao; 
+        const capturaHeight = videoWidth * proporcao;
 
         canvas.width = capturaWidth;
         canvas.height = capturaHeight;
-
+        const produtoSelecionado = document.getElementById('drop_produtos').value;
+        const clienteSelecionado = document.getElementById('drop_clientes').value;
         context.drawImage(video, 0, 0, capturaWidth, capturaHeight);
         const dados_imagem = canvas.toDataURL('image/jpg').replace('data:image/jpeg;base64,', '').replace('data:image/png;base64,', '');
 
         $.ajax({
             type: "POST",
             url: "/Webcan/SalvarFoto",
-            data: { dados_imagem: dados_imagem },
+            data: { dados_imagem: dados_imagem, produto_selecionado: produtoSelecionado, cliente_selecionado: clienteSelecionado },
             success: function (response) {
                 if (response.sucesso) {
                     alert(response.mensagem);
